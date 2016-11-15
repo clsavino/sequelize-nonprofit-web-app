fa150d48966ccb366b8524da17c01c2be34916cb
Christi Savino <c.savino@ymail.com>
2016-11-15 09:03:55 -0500

    added explanation in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,15 @@
 /*
-Make the connection to the databases and export
+Make the connection to the databases and exports
 */
+// Needed to see JawsDB database in MySQL workbench
+
 var mysql = require('mysql');
 var connection;
 
 if (process.env.JAWSDB_URL) {
+  //if the server contains the JAWSDB_URL environment variable, it connects to the JawsDB database, connection credentials are hidden in the environment variable
   connection = mysql.createConnection(process.env.JAWSDB_URL);
+  //else, it falls back on the explicitly defined local database
 } else {
     connection = mysql.createConnection({
     host: 'localhost',
